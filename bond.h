bb9f89f80ddac040e996d4ef39c6f746fd4c06a7
Rahul Kumar <93584728+rkstu@users.noreply.github.com>
2023-01-08 17:02:27 +0530

    Add files via upload

---
+++ bond.h
@@ -0,0 +1,23 @@
+// #include "stdafx.h" // uncomment if using Visual Studio
+#include <iostream>
+
+// // a header guard to protect against double inclusion
+// #ifndef BOND_H
+// #define BOND_H
+
+// our function prototypes
+double getParValue();
+int getNumOfCoupons();
+double getCouponRate();
+double getRequiredYield();
+double getMaturityYears();
+bool isZeroCoupon(int numCoupons);
+double getrequiredYieldPerPeriod(double yield, int numCoupons);
+double getcouponAmount(double couponRate, int numCoupons, double parValue);
+double getPeriods(double maturity, int numCoupons);
+double getBondPrice(double parValue, double couponAmount, double requiredyieldPerPeriod, double periods);
+double getZeroBondPrice(double parValue, double requiredyieldPerPeriod, double periods);
+void displayBondPrice(double bondPrice);
+void displayErrorMessage();
+
+// #endif
